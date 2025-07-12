9937286424e0b399dad78918bd6454eb3e41c672
Priyabrata Pattanaik <githubprojects100@gmail.com>
2025-07-12 23:27:35 +0530

    Auth Module Added

--- src/main/java/com/playschool/management/controller/AuthController.java
+++ src/main/java/com/playschool/management/controller/AuthController.java
@@ -1,11 +1,4 @@
-package cimport org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;school.management.controller;
+package com.playschool.management.controller;
 
 import java.util.HashSet;
 import java.util.List;
